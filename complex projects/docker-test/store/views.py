f2e2ee239cc7e31958a739c396d2e79eca0aec98
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2026-08-16 13:02:08 +0600

    It's my live portfolio

--- complex projects/docker-test/store/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from django.views import View
-from django.http import HttpResponse
-
-
-class HomeView(View):
-    def get(self, request):
-        return HttpResponse("Hello, World!")
--- complex projects/eid Mubarak/test.py
+++
@@ -1,8 +0,0 @@
-def twoSum(nums, target):
-    seen = {}
-    for i, num in enumerate(nums):
-        need = target - num
-        if need in seen:
-            return [seen[need], i]
-        seen[num] = i
-print()
